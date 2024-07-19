91aeb9f2a131759591203f50bb5b2cbe5719fa8f
george9907 <georgii.lytvyn2005@gmail.com>
2024-07-19 15:12:48 +0200

    status fix

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -13,8 +13,8 @@ export const initMongoConnection = async () => {
       `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`,
     );
     console.log('Mongo connection successfully established!');
-  } catch (e) {
-    console.log('Error while setting up mongo connection', e);
-    throw e;
+  } catch (error) {
+    console.log('Error while setting up mongo connection', error);
+    throw error;
   }
 };
--- src/server.js
+++ src/server.js
@@ -20,7 +20,6 @@ export const setupServer = () => {
     }),
   );
 
-
   app.get('/', (req, res) => {
     res.json({
       message: 'Hello User!',
@@ -30,8 +29,10 @@ export const setupServer = () => {
   app.get('/contacts', async (req, res) => {
     try {
       const contacts = await getAllContacts();
-  
+
       res.status(200).json({
+        status: 200,
+
         message: 'Successfully found contacts!',
         data: contacts,
       });
@@ -45,30 +46,35 @@ export const setupServer = () => {
       const contact = await getContactById(contactId);
       if (!contact) {
         res.status(404).json({
+          status: 404,
           message: 'Contact not found',
         });
         return;
       }
       res.status(200).json({
+        status: 200,
         message: `Successfully found contact with id ${contactId}!`,
         data: contact,
       });
     } catch (error) {
       res.status(404).json({
-          message: `Contact with id ${contactId} not found`,
-        });
-      // next(error); 
+        status: 404,
+        message: `Contact with id ${contactId} not found`,
+      });
+      // next(error);
     }
   });
 
   app.use('*', (req, res, next) => {
     res.status(404).json({
+      status: 404,
       message: 'Not found',
     });
   });
 
   app.use((err, req, res, next) => {
     res.status(500).json({
+      status: 500,
       message: 'Something went wrong',
       error: err.message,
     });
